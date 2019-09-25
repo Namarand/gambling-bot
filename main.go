29c6ea800fd0df5e802213b69ba7745be690aba1
Clément Galland <clement.galland.76@gmail.com>
2019-09-25 03:30:34 +0100

    init

---
+++ main.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+
+	"github.com/gempir/go-twitch-irc"
+)
+
+type Gambling struct {
+    IsOpen bool
+    Possibilities []string
+    Vote map[string]string
+}
+
+var vote *Gambling
+var client *twitch.Client
+var CHANNEL = "val_pl_magicarenafr"
+
+func isValid(str string) bool {
+    if vote == nil {
+	return false
+    }
+    for _, key := range vote.Possibilities {
+	if key == str {
+	    return true
+	}
+    }
+    return false
+}
+
+func sayAdmin(message string) {
+    client.Say(CHANNEL, "@" + CHANNEL + ": " + message)
+}
+
+func checkPermission(user twitch.User) bool {
+    return user.Name == "namarand" || user.Name == CHANNEL
+}
+
+func handleCreate(user twitch.User, contents []string) {
+    if !checkPermission(user) {
+	return
+    }
+    if vote != nil {
+	sayAdmin("There is already a vote going, you should delete it first with '!gamble delete'.")
+	return
+    }
+    if len(contents) <= 2 {
+	sayAdmin("You must pass the possibilities as arguments.")
+	return
+    }
+    vote = new(Gambling)
+    vote.IsOpen = true
+    vote.Vote = make(map[string]string)
+    vote.Possibilities = contents[2:]
+    sayAdmin("There is a new vote! You can vote with '!gamble vote <vote>'.")
+    sayAdmin("You can vote for: " + strings.Join(contents[2:], ", "))
+}
+
+func handleClose(user twitch.User) {
+    if !checkPermission(user) {
+	return
+    }
+    if vote == nil {
+	sayAdmin("There is no on going vote.")
+	return
+    }
+    vote.IsOpen = false
+    sayAdmin("The vote is now closed!")
+}
+
+func handleRoll(user twitch.User, contents []string) {
+    if !checkPermission(user) {
+	return
+    }
+    if vote == nil {
+	sayAdmin("There is no on going vote.")
+	return
+    }
+    if vote.IsOpen {
+	sayAdmin("The vote isn't closed.")
+	return
+    }
+    if len(contents) < 3 {
+	sayAdmin("You must specify the winner.")
+	return
+    }
+    if !isValid(contents[2]) {
+	sayAdmin("This is not a correct option: " + contents[2])
+    }
+    winners := []string{}
+    for user, vote := range vote.Vote {
+	if vote == contents[2] {
+	    winners = append(winners, user)
+	}
+    }
+    if len(winners) == 0 {
+	sayAdmin("No one vote for this one...")
+	return
+    }
+    sayAdmin("The winner is... " + winners[rand.Intn(len(winners))])
+}
+
+func handleDisplay(user twitch.User, contents []string) {
+    if !checkPermission(user) {
+	return
+    }
+    if vote == nil {
+	sayAdmin("There is no on going vote.")
+	return
+    }
+    if len(contents) < 3 {
+	sayAdmin("You must specify the categorie to display.")
+	return
+    }
+    if !isValid(contents[2]) {
+	sayAdmin("This is not a correct option: " + contents[2])
+    }
+    transformed := make(map[string][]string)
+    for user, value := range vote.Vote {
+	transformed[value] = append(transformed[value], user)
+    }
+    client.Say(CHANNEL, "The current vote for " + contents[2] + ": " + strings.Join(transformed[contents[2]], ", "))
+}
+
+func handleVote(user twitch.User, contents []string) {
+    if !vote.IsOpen {
+	fmt.Println("Don't try to vote while it's close...")
+	    return
+    }
+    if len(contents) < 2 {
+	fmt.Println("Expected one argument to vote...")
+    }
+    if isValid(contents[2]) {
+	vote.Vote[user.Name] = contents[2]
+    } else {
+	fmt.Println("You should vote for something that exist!")
+    }
+}
+
+func handleDelete(user twitch.User) {
+    if !checkPermission(user) {
+	return
+    }
+    vote = nil
+}
+
+func handleReset(user twitch.User) {
+    if !checkPermission(user) {
+	return
+    }
+    vote.Vote = make(map[string]string)
+}
+
+func parseMessage(message twitch.PrivateMessage) {
+    if !strings.HasPrefix(message.Message, "!gamble") {
+	return
+    }
+    contents := strings.Split(message.Message, " ")
+    if len(contents) == 1 {
+	return
+    }
+    switch contents[1] {
+    case "create":
+	handleCreate(message.User, contents)
+    case "close":
+	handleClose(message.User)
+    case "roll":
+	handleRoll(message.User, contents)
+    case "display":
+	handleDisplay(message.User, contents)
+    case "vote":
+	handleVote(message.User, contents)
+    case "delete":
+	handleDelete(message.User)
+    case "reset":
+	handleReset(message.User)
+    }
+}
+
+func main() {
+	client = twitch.NewClient("GamblingBotValPL", "oauth:9tcadjq1yafy4uqjzfrx3fm3d89yu3")
+
+	client.OnPrivateMessage(parseMessage)
+	client.OnConnect(func() {
+	    client.Say(CHANNEL, "Hi there! Ready to gamble?")
+	})
+
+	client.Join(CHANNEL)
+
+	err := client.Connect()
+	if err != nil {
+		panic(err)
+	}
+}
