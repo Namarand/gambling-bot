aa64478b1c5f174515cfa0d21eee9e6360c446b7
Wilfried OLLIVIER <wollivier@fdn.fr>
2020-04-08 14:43:26 +0200

    Bump version to 1.0.0

--- cmd/gambling-bot.go
+++ cmd/gambling-bot.go
@@ -18,7 +18,7 @@ func main() {
 	// Basic config
 	app.Name = "gambling-bot"
 	app.Usage = "A golang powered Twitch bot handling simple vote mechanism"
-	app.Version = "0.3.1"
+	app.Version = "1.0.0"
 
 	// Flags
 	app.Flags = []cli.Flag{
