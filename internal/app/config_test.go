260dda8d13e26aaf8c4a72c706bdb1cebc24e979
Wilfried OLLIVIER <wollivier@fdn.fr>
2020-04-08 14:40:32 +0200

    Remove useless test

--- internal/app/config_test.go
+++ internal/app/config_test.go
@@ -6,17 +6,12 @@ import (
 
 func TestGetConf(t *testing.T) {
 
-	var expectedKey string = "KEY"
 	var expectedToken string = "TOKEN"
 	var expectedAdminLen int = 3
 
 	var c Conf
 
 	c.getConf("../../tests/config.yml")
-
-	if c.Pastebin.Key != expectedKey {
-		t.Errorf("[c.Pastebin.key] Expected : %s, Get : %s", expectedKey, c.Pastebin.Key)
-	}
 
 	if c.Twitch.Oauth != expectedToken {
 		t.Errorf("[c.Twitch.Oauth] Expected : %s, Get : %s", expectedToken, c.Pastebin.Key)
